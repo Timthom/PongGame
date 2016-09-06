4e79079ce61909af25fa8037b89bf712ea60b169
Thomas Månsson <thomasmonsson@gmail.com>
2016-09-06 23:31:45 +0200

    Initial Commit

---
+++ PongGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PongGame
+//
+//  Created by Thomas on 2016-09-06.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PongGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PongGame
+//
+//  Created by Thomas on 2016-09-06.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
